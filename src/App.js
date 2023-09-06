c8d6c49274716d0c8cab6cac3beb35fc36d6d58f
anobes6416 <sebonaifa777@gmail.com>
2023-09-05 23:16:54 -0400

    component commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Hi from "./components/class_comp";
+import {Hello} from "./components/functional_comp";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and sebona to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello />
+      <Hi />
     </div>
   );
 }
---
+++ src/components/functional_comp.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// function Hello() {
+//     return <h1>functional component</h1>
+// }
+
+// export default Hello;
+
+    //OR
+
+// export const Hello = () => "functional component";
+
+    //OR
+
+import React from 'react';
+
+export function Hello(){
+    return <h1>functional component</h1>
+}
